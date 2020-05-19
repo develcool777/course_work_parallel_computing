0484c11be54d1deb26dadb7287f20aa36146e588
Вадим Павлюк <dyletant@ukr.net>
2020-05-19 13:30:32 +0300

    class LexemaList

--- main.py
+++ main.py
@@ -27,5 +27,33 @@ class Lexema:
       raise Exception('Lexema(A) + Lexema(B): undefind for diff A and B')
     return Lexema(self.text, self.frequence + item.frequence, [*self.docIds, *item.docIds])
 
-  def __eq__(self, item):
-    return str(self) == str(item)
+
+class LexemaList:
+  def __init__(self, items = []):
+    if type(items) != list:
+      raise Exception('LexemaList(not list) undefind')
+    self.items = list(filter(lambda item: type(item) == Lexema, items))
+    self.items.sort(key = lambda l: l.text)
+
+  def __repr__(self):
+    return ', \n'.join([repr(i) for i in self.items])
+
+  def __add__(self, items):
+    a = dict([[l.text, l] for l in self.items])
+    b = dict([[l.text, l] for l in items.items])
+    ka = list(a.keys())
+    kb = list(b.keys())
+    K = set(ka + kb)
+    def merge(k, a, b):
+      if k in a and k in b:
+        return a[k] + b[k]
+      elif k in a:
+        return a[k]
+      elif k in b:
+        return b[k]
+      else:
+        return None
+    return LexemaList([merge(k, a, b) for k in K])
+
+  def __eq__(self, items):
+    return str(self) == str(items)
